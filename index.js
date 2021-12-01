f097c5d16529e581e199a1633696366a726fd636
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-12-01 18:11:20 +0100

    update state, and some minor things

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 /*
-
 This is how an item object should look like
-
 {
   id: 1, // <- the item id matches the icon name in the assets/icons folder
   name: "beetroot",
   price: 0.35 // <- You can come up with your own prices
 }
-
 */
-
-let totalEl = document.querySelector('span.total-number')
-let totalPrice = 0
 
 const state = {
 
@@ -21,121 +15,137 @@ const state = {
       id: 1,
       name: 'beetroot',
       price: 0.97,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 2,
       name: 'carrot',
       price: 0.75,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 3,
       name: 'apple',
       price: 0.89,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 4,
       name: 'apricot',
       price: 0.55,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 5,
       name: 'avocado',
       price: 1.25,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 6,
       name: 'bananas',
       price: 1.57,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 7,
       name: 'bell-pepper',
       price: 2.55,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 8,
       name: 'berry',
       price: 2.90,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 9,
       name: 'blueberry',
       price: 4.50,
-      inCard: false
+      inCart: 0
     },
 
     {
       id: 10,
       name: 'eggplant',
       price: 5.75,
-      inCard: false
+      inCart: 0
     }
 
   ],
  
 }
 
-function renderStoreSvg(itemsParam) {
+//global variables wich i use in functions without passing as paraments, or in this case juse for total calculating
+let totalEl = document.querySelector('span.total-number')
+let totalPrice = 0
 
+//function to call the renderStoreItem for the header items to fill out in a for with argument an array from state
+function renderStoreSvg(itemsParam) {
+  //just has a for of loop, because i have an array parameter and want to call store header items
   for (const element of itemsParam) {
     renderStoreItem(element)
   }
 
 }
 
-function renderCardItem(cardImgParam) {
+//function to call renderCartItem this is called only when the btn in add to cart in renderStoreItem is clicked, problems here in rerendering
+function renderCartItem(cardImgParam) {
 
+  //creating the ul
   const ulEl = document.querySelector("main .item-list")
 
+  //creating the li
   const liEl = document.createElement('li')
 
+  //creating the img
   const imgEl = document.createElement('img')
   imgEl.setAttribute('class', 'cart--item-icon')
 
+  //checking if the id 10 will have the 0 not 00 cause then it doesnt load
   if (cardImgParam.id === 10) {
     imgEl.setAttribute('src', `assets/icons/0${cardImgParam.id}-${cardImgParam.name}.svg`)
   }
 
+  //other wich from id 1-9
   else {
     imgEl.setAttribute('src', `assets/icons/00${cardImgParam.id}-${cardImgParam.name}.svg`)
   }
 
-  imgEl.setAttribute('alt', 'beetroot')
+  imgEl.setAttribute('alt', `${cardImgParam.name}`)
 
+  //creating the p element
   const pEl = document.createElement('p')
 
+  //creating the btn element the first one with -
   const btnEl1 = document.createElement('button')
   btnEl1.setAttribute('class' , 'quantity-btn remove-btn center')
   btnEl1.textContent = '-'
   
+  //creating span element wich has the value when you change the btn + or -
   const spanEl = document.createElement('span')
   spanEl.setAttribute('class', 'quantity-text center')
   spanEl.textContent = '1'
 
+  //creating btn2 wich creates this button for +
   const btnEl2 = document.createElement('button')
   btnEl2.setAttribute('class', 'quantity-btn add-btn center')
   btnEl2.textContent = '+'
 
+  //appending things and ul is created totally
   liEl.append(imgEl, pEl, btnEl1, spanEl, btnEl2)
   ulEl.append(liEl)
 
-  //event listeners butttons
+  //event listeners butttons the 1 and 2 - and +
   btnEl1.addEventListener('click', function(event) {
 
     event.preventDefault()
@@ -148,6 +158,7 @@ function renderCardItem(cardImgParam) {
       liEl.remove()
     }
 
+    decreaseItemQuantity(cardImgParam)
     calculateTotalSub(cardImgParam)
 
   })
@@ -160,33 +171,50 @@ function renderCardItem(cardImgParam) {
     numberSpan += i
     spanEl.textContent = numberSpan
 
+    increaseItemQuantity(cardImgParam)
     calculateTotalAdd(cardImgParam)
 
   })
 
 }
 
+function decreaseItemQuantity(cardParam) {
+  cardParam.inCart -= 1
+}
+
+function increaseItemQuantity(cardParam) {
+  cardParam.inCart += 1
+}
+
+//this functions calls the above function in the btn eventlistener, and this renders all items in the header part the 10 elements in the store
 function renderStoreItem(imgParam) {
 
+  //we get the ul wich i want from header
   const ulEl = document.querySelector("header .item-list")
 
+  //creating li
   const liEl = document.createElement('li')
   
+  //creating a div
   const divEl = document.createElement('div')
   divEl.setAttribute('class', 'store--item-icon')
 
+  //creating an image
   const imgEl = document.createElement('img')
 
+  //checking that the id 10 should have different src image string
   if (imgParam.id === 10) {
     imgEl.setAttribute('src', `assets/icons/0${imgParam.id}-${imgParam.name}.svg`)
   }
 
+  //for the other from id 1-9 wich has an 00
   else {
     imgEl.setAttribute('src', `assets/icons/00${imgParam.id}-${imgParam.name}.svg`)
   }
 
-  imgEl.setAttribute('alt', 'beetroot')
+  imgEl.setAttribute('alt', `${imgParam.name}`)
 
+  //creating a button wich is crucial to this app
   const btnEl = document.createElement('button')
   btnEl.textContent = 'Add to cart'
 
@@ -194,20 +222,18 @@ function renderStoreItem(imgParam) {
   liEl.append(divEl, btnEl)
   ulEl.append(liEl)
 
-  //event listeners
+  //event listeners for the add to cart button
   btnEl.addEventListener('click', function(event) {
 
     event.preventDefault()
-    imgParam.inCard = true
-    if (imgParam.inCard === true) {
-      renderCardItem(imgParam)
-      calculateTotalAdd(imgParam)
-    }
+    renderCartItem(imgParam)
+    calculateTotalAdd(imgParam)
 
   })
   
 }
 
+//calculates when the small btn + is clicked and is called there in an event dom
 function calculateTotalAdd(itemsParam) {
 
   let price = itemsParam.price
@@ -216,6 +242,7 @@ function calculateTotalAdd(itemsParam) {
 
 }
 
+//calculates when the small btn - is clicked and is called there in an event dom
 function calculateTotalSub(itemsParam) {
 
   let price = itemsParam.price
@@ -224,8 +251,10 @@ function calculateTotalSub(itemsParam) {
 
 }
 
+//calls everything, rerenders the page and does all
 function render() {
   renderStoreSvg(state.items)
 }
 
+//the only function call in main, then everything renders here with function calls
 render()
