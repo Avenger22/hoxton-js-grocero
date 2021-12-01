d96464d58cca083abea824de994fe7eaa5555dcf
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-12-01 12:56:35 +0100

    templates rendered on page

--- index.js
+++ index.js
@@ -10,6 +10,55 @@ This is how an item object should look like
 
 */
 
+const state = {
+
+  items: [
+    {
+      id: 1,
+      name: 'beetroot',
+      price: 0.97
+    },
+
+    {
+      id: 2,
+      name: 'carrot',
+      price: 0.75
+    },
+
+    {
+      id: 3,
+      name: 'apple',
+      price: 0.89
+    },
+
+    {
+      id: 4,
+      name: 'appricot',
+      price: 0.55
+    }
+  ],
+ 
+}
+
+const imgSvg = [
+  '001-beetroot.svg',
+  '002-carrot.svg',
+  '003-apple.svg',
+  '004-apricot.svg',
+  '005-avocado.svg',
+  '006-bananas.svg',
+  '007-bell-pepper.svg',
+  '008-berry.svg',
+  '009-blueberry.svg',
+  '010-eggplant.svg',
+]
+
+function renderStoreSvg(imgSvgParam) {
+  for (const element of imgSvgParam) {
+    renderStoreItem(element)
+  }
+}
+
 function renderCardItem() {
 
   const ulEl = document.querySelector("main .item-list")
@@ -40,7 +89,7 @@ function renderCardItem() {
 
 }
 
-function renderStoreItem() {
+function renderStoreItem(imgParam) {
 
   const ulEl = document.querySelector("header .item-list")
 
@@ -50,7 +99,7 @@ function renderStoreItem() {
   divEl.setAttribute('class', 'store--item-icon')
 
   const imgEl = document.createElement('img')
-  imgEl.setAttribute('src', 'assets/icons/001-beetroot.svg')
+  imgEl.setAttribute('src', `assets/icons/${imgParam}`)
   imgEl.setAttribute('alt', 'beetroot')
 
   const btnEl = document.createElement('button')
@@ -62,5 +111,8 @@ function renderStoreItem() {
   
 }
 
-renderStoreItem()
-renderCardItem()
+function render() {
+  renderStoreSvg(imgSvg)
+}
+
+render()
