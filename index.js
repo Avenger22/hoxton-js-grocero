7a4db969902569bbb49d1b6de4856dfa192bba4a
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-12-03 12:38:42 +0100

    This version is with lecture update, the previous commit was my final update before the lecture

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-/*
-This is how an item object should look like
-{
-  id: 1, // <- the item id matches the icon name in the assets/icons folder
-  name: "beetroot",
-  price: 0.35 // <- You can come up with your own prices
-}
-*/
+//global variables wich i use in functions without passing as paraments, or in this case juse for total calculating
+let totalEl = document.querySelector('span.total-number')
+const storeUl = document.querySelector("header .item-list")
+const cartUl = document.querySelector("main .item-list")
 
 //state object, has an array items wich itself has lots of objects inside with 4 properties wich are updated and used in the app
 const state = {
@@ -86,19 +82,47 @@ const state = {
  
 }
 
-//global variables wich i use in functions without passing as paraments, or in this case juse for total calculating
-let totalEl = document.querySelector('span.total-number')
-let totalPrice = 0
-const storeUl = document.querySelector("header .item-list")
-const cartUl = document.querySelector("main .item-list")
+//HELPER FUNCTIONS
+
+function getCartItems(cartItemParam) {
+  return state.items.filter(item => item.inCart > 0)
+}
+
+//this is called when i click the small btn minus in cart item section, and updates the states
+function decreaseItemQuantity(cardParam) {
+  cardParam.inCart--
+}
+
+//this is called when i click the small btn plus in cart item section, and updates the states
+function increaseItemQuantity(cardParam) {
+  cardParam.inCart++
+}
+
+function calculateTotal() {
+
+  let total = 0
+
+  const cartArray = getCartItems()
+
+  for (const element of cartArray) {
+    total += element.price * element.inCart
+  }
+
+  return total
+
+}
+
+// RENDER FUNCTIONS
 
 //function to call the renderStoreItem for the header items to fill out in a for with argument an array from state
 function renderStore(itemsParam) {
+
   //just has a for of loop, because i have an array parameter and want to call store header items
 
-  //we get the ul wich i want from header
+  //we get the ul wich i want from header DESTROY THEN RECREATE
   storeUl.innerHTML = ''
 
+  //we create here individual store item so 10 will be shown each time you rerender and destroy then render etc
   for (const element of itemsParam) {
     renderStoreItem(element)
   }
@@ -106,7 +130,7 @@ function renderStore(itemsParam) {
 }
 
 //this functions calls the above function in the btn eventlistener, and this renders all items in the header part the 10 elements in the store
-function renderStoreItem(imgParam) {
+function renderStoreItem(storeParam) {
 
   //creating li
   const liEl = document.createElement('li')
@@ -119,16 +143,16 @@ function renderStoreItem(imgParam) {
   const imgEl = document.createElement('img')
 
   //checking that the id 10 should have different src image string
-  if (imgParam.id === 10) {
-    imgEl.setAttribute('src', `assets/icons/0${imgParam.id}-${imgParam.name}.svg`)
+  if (storeParam.id === 10) {
+    imgEl.setAttribute('src', `assets/icons/0${storeParam.id}-${storeParam.name}.svg`)
   }
 
   //for the other from id 1-9 wich has an 00
   else {
-    imgEl.setAttribute('src', `assets/icons/00${imgParam.id}-${imgParam.name}.svg`)
+    imgEl.setAttribute('src', `assets/icons/00${storeParam.id}-${storeParam.name}.svg`)
   }
 
-  imgEl.setAttribute('alt', imgParam.name)
+  imgEl.setAttribute('alt', storeParam.name)
 
   //creating a button wich is crucial to this app
   const btnEl = document.createElement('button')
@@ -143,19 +167,34 @@ function renderStoreItem(imgParam) {
 
     event.preventDefault()
 
-    increaseItemQuantity(imgParam)
-    renderCartItem(imgParam)
-    
-    calculateTotalAdd(imgParam)
+    increaseItemQuantity(storeParam)
+    calculateTotal()
 
     render()
 
-  }, { once: true })
+  })
   
 }
 
+//this function render the cart in the main html
+function renderCart() {
+
+  //just has a for of loop, because i have an array parameter and want to call store header items
+
+  //we get the ul wich i want from header DESTROY
+  cartUl.innerHTML = ''
+
+  // then, recreate the contents of the cart FILTER ARRAY THIS RETURNED THING
+  const cart = getCartItems() //KEY TO MAKE THINGS WORK
+
+  for (const element of cart) {
+    renderCartItem(element)
+  }
+
+}
+
 //function to call renderCartItem this is called only when the btn in add to cart in renderStoreItem is clicked, problems here in rerendering
-function renderCartItem(cardImgParam) {
+function renderCartItem(cartParam) {
 
   //creating the li
   const liEl = document.createElement('li')
@@ -165,16 +204,16 @@ function renderCartItem(cardImgParam) {
   imgEl.setAttribute('class', 'cart--item-icon')
 
   //checking if the id 10 will have the 0 not 00 cause then it doesnt load
-  if (cardImgParam.id === 10) {
-    imgEl.setAttribute('src', `assets/icons/0${cardImgParam.id}-${cardImgParam.name}.svg`)
+  if (cartParam.id === 10) {
+    imgEl.setAttribute('src', `assets/icons/0${cartParam.id}-${cartParam.name}.svg`)
   }
 
   //other wich from id 1-9
   else {
-    imgEl.setAttribute('src', `assets/icons/00${cardImgParam.id}-${cardImgParam.name}.svg`)
+    imgEl.setAttribute('src', `assets/icons/00${cartParam.id}-${cartParam.name}.svg`)
   }
 
-  imgEl.setAttribute('alt', `${cardImgParam.name}`)
+  imgEl.setAttribute('alt', `${cartParam.name}`)
 
   //creating the p element
   const pEl = document.createElement('p')
@@ -187,7 +226,7 @@ function renderCartItem(cardImgParam) {
   //creating span element wich has the value when you change the btn + or -
   const spanEl = document.createElement('span')
   spanEl.setAttribute('class', 'quantity-text center')
-  spanEl.textContent = cardImgParam.inCart
+  spanEl.textContent = cartParam.inCart
 
   //creating btn2 wich creates this button for +
   const btnEl2 = document.createElement('button')
@@ -202,15 +241,15 @@ function renderCartItem(cardImgParam) {
   btnEl1.addEventListener('click', function(event) {
 
     event.preventDefault()
-    decreaseItemQuantity(cardImgParam)
+    decreaseItemQuantity(cartParam)
 
-    spanEl.textContent = cardImgParam.inCart
+    spanEl.textContent = cartParam.inCart
 
-    if (cardImgParam.inCart === 0) {
-      liEl.remove()
-    }
+    // if (cardImgParam.inCart === 0) {
+    //   liEl.remove() YOU DONT NEED THIS YOU HAVE STATE NO DOM
+    // }
 
-    calculateTotalSub(cardImgParam)
+    calculateTotal()
     render()
 
   })
@@ -218,49 +257,32 @@ function renderCartItem(cardImgParam) {
   btnEl2.addEventListener('click', function(event) {
 
     event.preventDefault()
-    increaseItemQuantity(cardImgParam)
+    increaseItemQuantity(cartParam)
 
-    spanEl.textContent = cardImgParam.inCart
+    spanEl.textContent = cartParam.inCart
 
-    calculateTotalAdd(cardImgParam)
+    calculateTotal()
     render()
 
   })
 
 }
 
-//this is called when i click the small btn minus in cart item section, and updates the states
-function decreaseItemQuantity(cardParam) {
-  cardParam.inCart -= 1
-}
+function renderTotal() {
 
-//this is called when i click the small btn plus in cart item section, and updates the states
-function increaseItemQuantity(cardParam) {
-  cardParam.inCart += 1
-}
-
-//calculates when the small btn + is clicked and is called there in an event dom
-function calculateTotalAdd(itemsParam) {
-
-  let price = itemsParam.price
-  totalPrice = totalPrice + price
-  totalEl.textContent = `£${totalPrice.toFixed(2)}`
-
-}
-
-//calculates when the small btn - is clicked and is called there in an event dom
-function calculateTotalSub(itemsParam) {
-
-  let price = itemsParam.price
-  totalPrice = totalPrice - price
-  totalEl.textContent = `£${totalPrice.toFixed(2)}`
+  totalEl.textContent = '£' + calculateTotal().toFixed(2)
 
 }
 
 //calls everything, rerenders the page and does all
 function render() {
+
   renderStore(state.items)
+  renderCart()
+  renderTotal()
+
 }
 
+//FUNCTION CALL
 //the only function call in main, then everything renders here with function calls
 render()
